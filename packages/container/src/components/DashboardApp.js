b2586955b0e297ac8eb4f5336eaf87f0550c486e
kiran c chand <kirancchand@gmail.com>
2024-04-03 14:25:55 +0530

    remodified dashboard 2

--- packages/container/src/components/DashboardApp.js
+++ packages/container/src/components/DashboardApp.js
@@ -1,11 +1,12 @@
-import {mount} from 'dashboard/DashboardApp';
-import React,{useRef,useEffect} from 'react';
+import { mount } from 'dashboard/DashboardApp';
+import React, { useRef, useEffect } from 'react';
 
-export default()=>{
-    const ref=useRef(null);
-    useEffect(()=>{
-        mount(ref.current)
-           
-    },[]);
-    return <div ref={ref} />;
-}
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return <div ref={ref} />;
+};
