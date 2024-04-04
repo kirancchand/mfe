36cb26b7bf70bd8c2c22229d1564dc5806c73bfa
kiran c chand <kirancchand@gmail.com>
2024-04-04 14:46:31 +0530

    dashboard content removed 10

--- packages/dashboard/src/components/Dashboard.vue
+++ packages/dashboard/src/components/Dashboard.vue
@@ -2,6 +2,8 @@
   <div>
     <span>######Users######## my ######user!!!! ######</span>
     <span>######Users######## my ######user!!!! ######</span>
+    <span>######Users######## my ######user!!!! ######</span>
+    <span>######Users######## my ######user!!!! ######</span>
   <!-- <div class="p-grid p-fluid dashboard">
     <div class="p-col-12 p-lg-4">
       <div class="card summary">
